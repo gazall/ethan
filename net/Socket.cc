9c4059b656ea4f8bae25efaa98e478da522b124c
gazall <liulizhi10@sina.cn>
2019-11-21 22:46:54 +0800

    DE: class Socket

--- net/Socket.cc
+++ net/Socket.cc
@@ -1,1 +1,56 @@
 #include "Socket.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+
+using namespace std;
+
+Socket::Socket(int family_, int type_, int protocol_) :
+    family(family_), type(type_), protocol(protocol_) {
+}
+
+Socket::~Socket() {
+    if (!isFdValid()) {
+        return ;
+    }
+
+    ::close(fd);
+}
+
+void Socket::bindAndListen(Address::ptr addr) {
+    fd = socket(family, type, protocol); 
+    if (fd < 0) {
+        throw  
+    }
+
+    localAddr = addr; 
+    int ret = bind(fd, localAddr->getAddr(), localAddr->getAddrLen());
+    if (ret < 0) {
+        throw
+    }
+
+    ret = listen(fd, SOMAXCONN);
+    if (ret < 0) {
+        throw
+    }
+}
+
+Socket::ptr Socket::accept() {
+
+}
+
+void Socket::connect() {
+
+}
+
+bool Socket::isFdValid() {
+
+}
+
+int Socket::send(const char *buf, size_t len, int flag = 0) {
+
+}
+
+int Socket::recv(char *buf, size_t len, int flag = 0) {
+
+}
+
--- net/Socket.h
+++ net/Socket.h
@@ -16,29 +16,35 @@ public:
     typedef std::shared_ptr<Socket> ptr;
     typedef std::weak_ptr<Socket> weak_ptr;
 
-    Socket(int family, int type, int protocol, Address::ptr address); 
+    Socket(int family, int type, int protocol); 
     ~Socket();
     Socket(const Socket &other) = delete;
     Socket &operator=(const Socket &other) = delete;
 
-    void listenAndBind();
+    void bindAndListen(Address::ptr localAddr);
     Socket::ptr accept();
     void connect();
 
     int send(const char *buf, size_t len, int flag = 0);
     int recv(char *buf, size_t len, int flag = 0);
 
+    void setFd(int acceptFd) {
+        fd = acceptFd;
+    }
+
+    bool isFdValid();
+
     bool isConnected() {
         return isConnected;
     }
 private:
-    int fd; 
-    int family;
-    int type;
-    int protocol;
-    bool isConnected;
-    Address::ptr localAddr; 
-    Address::ptr remoteAddr; 
+    int fd = -1; 
+    int family = 0;
+    int type = 0;
+    int protocol = 0;
+    bool isConnected = false;
+    Address::ptr localAddr = nullptr; 
+    Address::ptr remoteAddr = nullptr; 
 };
 
 }
