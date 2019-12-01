f4d4f9d543295dd892ef4e0bfc12d75d8fb2cc9f
gazall <liulizhi10@sina.cn>
2019-12-01 17:41:43 +0800

    DE: Solve issue #1

--- test/log_test.cc
+++ test/log_test.cc
@@ -22,14 +22,13 @@ int main() {
     pthread_t t1, t2;
     pthread_create(&t1, NULL, &info, NULL);
     pthread_create(&t2, NULL, &debug, NULL);
+    std::thread t3(info1);
+    std::thread t4(debug1);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-
-    std::thread t3(info1);
-    std::thread t4(debug1);
     t3.join();
     t4.join();
-
+    
     return 0;
 }
